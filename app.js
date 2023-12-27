9f9a48167dda8567fae4e2c5e6c31bc05e66a5fb
nemo6 <miguel.maudet.contact@gmail.com>
2023-12-27 06:26:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-const fs      = require('fs')
-const http    = require('http')
-const path    = require('path')
-const hash    = require('crypto').createHash
-const sqlite  = require('sqlite')
-const sqlite3 = require('sqlite3')
-const dayjs   = require('dayjs')
+let user = require("os").userInfo().username
+
+const fs      = require("fs")
+const http    = require("http")
+const path    = require("path")
+const hash    = require("crypto").createHash
+const sqlite  = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/sqlite`)
+const sqlite3 = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/sqlite3`)
+const dayjs   = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/dayjs`)
 const _       = init()
 const PORT    = 8080
-// test
-function btime(x){
-	return dayjs(Math.trunc(x/1000+Date.UTC(1601,0,1))).format("dddd DD MMMM YYYY HH:mm:ss")
-}
 
-list_pathx = [
-"C:\\Users\\USERNAME\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History",
-]
+let pc_name = require("os").hostname().toLowerCase()
+
+;( async () => {
+
+	if( !fs.readdirSync("C:/test").includes("History") ){
+
+		fs.copyFileSync(`C:/Users/${user}/AppData/Local/Google/Chrome/User Data/Default/History`,
+		"C:/test/History")
+
+	}
+
+	let a = await search("C:/test/History")
+
+	//
+
+	let b = fs.readFileSync("C:/test/test.txt","utf8").split("\n")
+
+	//
+
+	let t = JSON.parse( b.at(-1) ).time
+
+	//
+
+	a = a.filter( x => x.time >= t )
+
+	a = a.map( x => ({ title:hexEncode(x.title),url:x.url,time:x.time,source:pc_name }) )
+
+	a = a.map( x => JSON.stringify(x) )
+
+	a = _.difference(a,b)
+
+	/*for( let x of a )
+	fs.appendFileSync("C:/test/test.txt", "\n" + x )*/
+
+	const stream = fs.createWriteStream( "C:/test/test.txt" , { flags: "a" } )
+	
+	for( let x of a ) stream.write( "\n" + x, "utf8" )
+	stream.end()
+
+	setTimeout(function () {
+	fs.unlinkSync("C:/test/History")
+	}, 2000 )
+
+})()
 
 async function search(x){
 
@@ -28,195 +67,46 @@ async function search(x){
 
 	let max = rows.length
 
-	for ( [i,row] of rows.entries() ){
+	for ( let [i,row] of rows.entries() ){
 
+		if( row.last_visit_time != 0 && row.hidden != 1 )
 		tableau.push({
 		"title" : row.title,
 		"url":row.url,
 		"time": row.last_visit_time,
-		"position":i,
-		"id": row.id,
-		"hidden": row.hidden
 		})
 
 	}
 
-	tableau.sort( (a,b) => b.time - a.time )
+	tableau.sort( (a,b) => a.time - b.time )
 
 	return tableau
 
 }
 
-const convert = (from, to) => str => Buffer.from(str, from).toString(to)
-const hexEncode = convert('utf8', 'hex')
-const hexDecode = convert('hex', 'utf8')
+function hexEncode(x){
+	if( x === "" ) return ""
+	let URI = encodeURIComponent(x).match(/\%.{2}|./g)
+	.map( x => x[0] != "%" ? x.charCodeAt(0).toString(16) : x )
+	.map( x => x.toLowerCase().replace("%","") )
+	return URI.join("")
+}
 
-;(async () => {
+function hexDecode(x){
+	if( x === "" ) return ""
+	if( x.length % 2 != 0 ) throw "length is not valid"
+	let hexes = x.match(/.{2}/g)
+	return decodeURIComponent( hexes.map( x => "%"+x ).join("") )
+}
 
-	if( !fs.readdirSync("C:\\test").includes("History") ){
-
-		fs.copyFileSync("C:\\Users\\USERNAME\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History",
-		"C:\\test\\History")
-
-	}
-
-	let a = await search("C:\\test\\History")
-
-	a = a.filter( x=> x.time != 0 && x.hidden != 1 )
-
-	//
-
-	b = fs.readFileSync("C:\\test\\test.txt","utf8").split("\n")
-
-	//
-
-	a = a.filter( x => x.time >= JSON.parse(b[0]).time )
-
-	a = a.map( x => ({ title:hexEncode(x.title),url:x.url,time:x.time }) )
-
-	a = strArr(a)
-
-	aa = _.difference(a,b)
-
-	for( x of aa.reverse() )
-	fs.appendFileSync("C:\\test\\test.txt", x + "\n" ) // C7PdeluC15k
-
-	setTimeout(function () {
-	fs.unlinkSync("C:\\test\\History")
-	}, 2000)
-
-})()
-
-function strArr(x){
-
-	let arr = []
-	for( y of x )
-	arr.push(JSON.stringify(y))
-	return arr
+function btime(x){
+	return dayjs(Math.trunc(x/1000+Date.UTC(1601,0,1))).format("dddd DD MMMM YYYY HH:mm:ss")
 }
 
 function init(){
 
-	String.prototype.short = function(n){
-
-		if( this.length > n )
-		str = this.slice(0,n) + "..."
-		else
-		str = this
-		return str
+	require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/dayjs/locale/fr`)
+	dayjs.locale("fr")
 	
-	}
-
-	String.prototype.server = function(n) {
-
-		let v = this.valueOf()
-
-		const http = require("http")
-		const PORT = 8080
-
-		http.createServer(function (req, res) {
-			
-			res.writeHead(200,{"content-type":`text/${n};charset=utf8`})
-
-			res.end(v)
-
-		}).listen(PORT)
-
-		console.log(`Running at port ${PORT}`)
-
-	}
-
-	Array.prototype.renderRange = function(n,m){
-
-		let STYLE = "<style>table{font-family:arial,sans-serif;border-collapse:collapse;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}</style>"
-
-		if( !Array.isArray(this) )
-		  return "not an array"
-
-		if( m == undefined ){
-
-			if( n > 0 )
-			{min=0;max=n}
-			else
-			{n=(-n);min=this.length-n;max=this.length}
-
-		}else{
-
-			min=n;max=m
-		}
-
-		let content   = "<table>"
-		let grandient = "background:repeating-linear-gradient(45deg,#606dbc,#606dbc 10px,#465298 10px,#465298 20px);"
-		
-		for( let i=min;i<max+1;i++ ){
-			try { 
-				let style = this[i]?.hidden == 1 ? 0.5 : 1
-				content += `<tr style="opacity:${style};"><td>${i}</td><td style="">${_.escape(this[i].title).short(67)}</td><td>${btime(this[i].time)}</td>`
-			}catch(e){
-				content += `<tr><td>${i}</td><td style="${grandient}">&#8203</td><td>&#8203</td>`
-			}
-		}
-		content += "</table>"
-		content += STYLE
-		return content
-	}
-
-	Object.prototype.str = function(n){
-
-		if( n == 1 )
-		return "<pre>"+JSON.stringify(this,null,2)+"</pre>"
-		else
-		return JSON.stringify(this,null,2)
-	}
-	
-	require('C:/Users/USERNAME/AppData/Roaming/npm/node_modules/dayjs/locale/fr')
-	
-	dayjs.locale('fr')
-	
-	return require('C:/Users/USERNAME/AppData/Roaming/npm/node_modules/lodash')
-  
+	return require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/lodash`)
 }
-
-;(async () => {
-
-	if( !fs.readdirSync(__dirname).includes("History") ){
-
-		fs.copyFileSync("C:\\Users\\USERNAME\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History",
-		"C:\\Users\\USERNAME\\Desktop\\test_hs\\History")
-
-	}
-
-	let a = await search("History")
-
-	a = a.filter( x=> x.time != 0 && x.hidden != 1 )
-
-	for(i=12431;i>=0;i--){
-
-		console.log(i)
-		let str =  { title:hexEncode(a[i].title),url:a[i].url,time:a[i].time }
-		fs.appendFileSync( "test.txt", JSON.stringify(str) + "\n" )
-	}
-
-	// setTimeout(function () {
-	// console.log('boo')
-	// fs.unlinkSync("History")
-	// }, 2000)
-
-})
-
-;(async () => {
-	
-	let a = await search(1)
-
-	a = a.filter( x=> x.time != 0 && x.hidden != 1 )
-
-	console.log(a.length)
-
-	let n = 200
-	let m = 12000
-
-	a.
-	renderRange( (a.length-m)-n, a.length-m )
-	.server("html")
-
-})
