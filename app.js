904935ea629a25978f1c2e72089e05e31eafefe7
nemo6 <miguel.maudet.contact@gmail.com>
2024-05-04 15:47:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let user = require("os").userInfo().username
+let USERNAME = require("os").userInfo().username
 
 const fs      = require("fs")
 const http    = require("http")
 const path    = require("path")
 const hash    = require("crypto").createHash
-const sqlite  = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/sqlite`)
-const sqlite3 = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/sqlite3`)
-const dayjs   = require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/dayjs`)
+const sqlite  = require(`C:/Users/${USERNAME}/AppData/Roaming/npm/node_modules/sqlite`)
+const sqlite3 = require(`C:/Users/${USERNAME}/AppData/Roaming/npm/node_modules/sqlite3`)
+const dayjs   = require(`C:/Users/${USERNAME}/AppData/Roaming/npm/node_modules/dayjs`)
 const _       = init()
 const PORT    = 8080
 
@@ -14,99 +14,99 @@ let pc_name = require("os").hostname().toLowerCase()
 
 ;( async () => {
 
-	if( !fs.readdirSync("C:/test").includes("History") ){
+    if( !fs.readdirSync("C:/test").includes("History") ){
 
-		fs.copyFileSync(`C:/Users/${user}/AppData/Local/Google/Chrome/User Data/Default/History`,
-		"C:/test/History")
+        fs.copyFileSync(`C:/Users/${USERNAME}/AppData/Local/Google/Chrome/User Data/Default/History`,
+        "C:/test/History")
 
-	}
+    }
 
-	let a = await search("C:/test/History")
+    let a = await search("C:/test/History")
 
-	//
+    //
 
-	let b = fs.readFileSync("C:/test/test.txt","utf8").split("\n")
+    let b = fs.readFileSync("C:/test/test.txt","utf8").split("\n")
 
-	//
+    //
 
-	let t = JSON.parse( b.at(-1) ).time
+    let t = JSON.parse( b.at(-1) ).time
 
-	//
+    //
 
-	a = a.filter( x => x.time >= t )
+    a = a.filter( x => x.time >= t )
 
-	a = a.map( x => ({ title:hexEncode(x.title),url:x.url,time:x.time,source:pc_name }) )
+    a = a.map( x => ({ title:hexEncode(x.title),url:x.url,time:x.time,source:pc_name }) )
 
-	a = a.map( x => JSON.stringify(x) )
+    a = a.map( x => JSON.stringify(x) )
 
-	a = _.difference(a,b)
+    a = _.difference(a,b)
 
-	/*for( let x of a )
-	fs.appendFileSync("C:/test/test.txt", "\n" + x )*/
+    /*for( let x of a )
+    fs.appendFileSync("C:/test/test.txt", "\n" + x )*/
 
-	const stream = fs.createWriteStream( "C:/test/test.txt" , { flags: "a" } )
-	
-	for( let x of a ) stream.write( "\n" + x, "utf8" )
-	stream.end()
+    const stream = fs.createWriteStream( "C:/test/test.txt" , { flags: "a" } )
+    
+    for( let x of a ) stream.write( "\n" + x, "utf8" )
+    stream.end()
 
-	setTimeout(function () {
-	fs.unlinkSync("C:/test/History")
-	}, 2000 )
+    setTimeout(function () {
+      fs.unlinkSync("C:/test/History")
+    }, 2000 )
 
 })()
 
 async function search(x){
 
-	let tableau = []
+    let table = []
 
-	let db = await sqlite.open({filename:x,
-	driver: sqlite3.Database
-	})
+    let db = await sqlite.open({filename:x,
+    driver: sqlite3.Database
+    })
 
-	let rows = await db.all("SELECT * FROM urls")
+    let rows = await db.all("SELECT * FROM urls")
 
-	let max = rows.length
+    let max = rows.length
 
-	for ( let [i,row] of rows.entries() ){
+    for ( let [i,row] of rows.entries() ){
 
-		if( row.last_visit_time != 0 && row.hidden != 1 )
-		tableau.push({
-		"title" : row.title,
-		"url":row.url,
-		"time": row.last_visit_time,
-		})
+        if( row.last_visit_time != 0 && row.hidden != 1 )
+        table.push({
+        "title" : row.title,
+        "url":row.url,
+        "time": row.last_visit_time,
+        })
 
-	}
+    }
 
-	tableau.sort( (a,b) => a.time - b.time )
+    table.sort( (a,b) => a.time - b.time )
 
-	return tableau
+    return table
 
 }
 
 function hexEncode(x){
-	if( x === "" ) return ""
-	let URI = encodeURIComponent(x).match(/\%.{2}|./g)
-	.map( x => x[0] != "%" ? x.charCodeAt(0).toString(16) : x )
-	.map( x => x.toLowerCase().replace("%","") )
-	return URI.join("")
+    if( x === "" ) return ""
+    let URI = encodeURIComponent(x).match(/\%.{2}|./g)
+    .map( x => x[0] != "%" ? x.charCodeAt(0).toString(16) : x )
+    .map( x => x.toLowerCase().replace("%","") )
+    return URI.join("")
 }
 
 function hexDecode(x){
-	if( x === "" ) return ""
-	if( x.length % 2 != 0 ) throw "length is not valid"
-	let hexes = x.match(/.{2}/g)
-	return decodeURIComponent( hexes.map( x => "%"+x ).join("") )
+    if( x === "" ) return ""
+    if( x.length % 2 != 0 ) throw "length is not valid"
+    let hexes = x.match(/.{2}/g)
+    return decodeURIComponent( hexes.map( x => "%"+x ).join("") )
 }
 
 function btime(x){
-	return dayjs(Math.trunc(x/1000+Date.UTC(1601,0,1))).format("dddd DD MMMM YYYY HH:mm:ss")
+    return dayjs(Math.trunc(x/1000+Date.UTC(1601,0,1))).format("dddd DD MMMM YYYY HH:mm:ss")
 }
 
 function init(){
 
-	require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/dayjs/locale/fr`)
-	dayjs.locale("fr")
-	
-	return require(`C:/Users/${user}/AppData/Roaming/npm/node_modules/lodash`)
+    require(`C:/Users/${USERNAME}/AppData/Roaming/npm/node_modules/dayjs/locale/fr`)
+    dayjs.locale("fr")
+    return require(`C:/Users/${USERNAME}/AppData/Roaming/npm/node_modules/lodash`)
+
 }
